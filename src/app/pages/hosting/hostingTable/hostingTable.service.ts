e1da348e32a1d8779147ac01bc9b3136fab2473c
Dany Van der Meij <xbit.dany@gmail.com>
2018-05-19 14:16:01 +0200

    feat(data): add_hosting_data

--- src/app/pages/hosting/hostingTable/hostingTable.service.ts
+++ src/app/pages/hosting/hostingTable/hostingTable.service.ts
@@ -11,6 +11,24 @@ export class HostingTableService {
       amount: 20,
       startdate: '27.06.2018',
       enddate: '27.06.2019'
+    },
+    {
+      image: 'assets/img/miners/antminers9.png',
+      name: 'Antminer L3+',
+      watt: 800,
+      price: 13.5,
+      amount: 20,
+      startdate: '27.06.2018',
+      enddate: '27.06.2019'
+    },
+    {
+      image: 'assets/img/miners/antminers9.png',
+      name: 'Antminer D3',
+      watt: 900,
+      price: 11,
+      amount: 20,
+      startdate: '27.06.2018',
+      enddate: '27.06.2019'
     }
   ];
 }
