e24b6d63f54060507e1578b7d356380a43f06fdd
Dany Van der Meij <xbit.dany@gmail.com>
2018-08-30 19:49:05 +0200

    chore(delete): delete_useless_pages

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -23,14 +23,7 @@ export const routes: Routes = [
       { path: 'miners', loadChildren: './miners/miners.module#MinersModule' },
       { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
       { path: 'hosting', loadChildren: './hosting/hosting.module#HostingModule' },
-      { path: 'balances', loadChildren: './balances/balances.module#BalancesModule' },
-      { path: 'editors', loadChildren: './editors/editors.module#EditorsModule' },
-      { path: 'components', loadChildren: './components/components.module#ComponentsModule' },
-      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-      { path: 'ui', loadChildren: './ui/ui.module#UiModule' },
-      { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
-      { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-      { path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
+      { path: 'balances', loadChildren: './balances/balances.module#BalancesModule' }
     ]
   }
 ];
