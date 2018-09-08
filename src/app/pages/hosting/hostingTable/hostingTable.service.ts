43d16f1cbe6042b2ee47f24cc589558d3f8d5ec9
Dany Van der Meij <xbit.dany@gmail.com>
2018-09-08 19:27:43 +0200

    feat(hostingtable): bind data to hostingtable

--- src/app/pages/hosting/hostingTable/hostingTable.service.ts
+++ src/app/pages/hosting/hostingTable/hostingTable.service.ts
@@ -1,34 +1,53 @@
 import { Injectable } from '@angular/core';
+import { MinerType } from '../../../entities/XBitApi/minertype';
+import { Miner } from '../../../entities/XBitApi/miner';
+import { HostingPeriod } from '../../../entities/XBitApi/hostingperiod';
+import { DataService } from '../../../services/data/data.service';
 
 @Injectable()
 export class HostingTableService {
-  hostingTableData = [
-    {
-      image: 'assets/img/miners/antminers9.png',
-      name: 'Antminer S9',
-      watt: 1300,
-      price: 13.5,
-      amount: 20,
-      startdate: '27.06.2018',
-      enddate: '27.06.2019'
-    },
-    {
-      image: 'assets/img/miners/antminers9.png',
-      name: 'Antminer L3+',
-      watt: 800,
-      price: 13.5,
-      amount: 20,
-      startdate: '27.06.2018',
-      enddate: '27.06.2019'
-    },
-    {
-      image: 'assets/img/miners/antminers9.png',
-      name: 'Antminer D3',
-      watt: 900,
-      price: 11,
-      amount: 20,
-      startdate: '27.06.2018',
-      enddate: '27.06.2019'
-    }
-  ];
+
+  minerTypes: MinerType[] = [];
+  miners: Miner[] = [];
+  hostingPeriods: HostingPeriod[] = [];
+
+  constructor(
+    private dataService: DataService
+  ) {
+    dataService.minerTypes.subscribe( res => {
+      this.minerTypes = res;
+    });
+    dataService.miners.subscribe( res => {
+      this.miners = res;
+    });
+    dataService.hostingPeriods.subscribe( res => {
+      this.hostingPeriods = res;
+    })
+  }
+
+  getHostingTableData() {
+    let hostingTableData: any = [];
+    this.hostingPeriods.forEach(element => {
+      hostingTableData.push({
+        image: 'assets/img/miners/antminers9.png',
+        name: this.getMinerTypeByMinerId(element.minerId).name,
+        watt: this.getMinerTypeByMinerId(element.minerId).watts,
+        price: element.pricePerMonth,
+        amount: 1,
+        startdate: element.startDate,
+        enddate: element.endDate
+      });
+    });
+    return hostingTableData;
+  }
+
+  private getMinerCountByMinerTypeId(pid: string) {
+    return this.miners.filter(x => x.minerTypeId == pid).length;
+  }
+
+  private getMinerTypeByMinerId(pMinerId: string): MinerType {
+    let pminer = this.miners.filter(x => x.id == pMinerId)[0];
+    return this.minerTypes.filter(x => x.id == pminer.minerTypeId)[0];
+  }
+
 }
