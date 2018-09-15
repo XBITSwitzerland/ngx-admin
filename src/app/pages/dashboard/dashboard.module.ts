86f76246c315cee8a32156ef90950769a196d9b4
Dany Van der Meij <xbit.dany@gmail.com>
2018-09-15 22:14:34 +0200

    fix(error): fix error

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -12,14 +12,8 @@ import { PieChart } from './pieChart';
 import { TrafficChart } from './trafficChart';
 import { UsersMap } from './usersMap';
 import { LineChart } from './lineChart';
-import { Feed } from './feed';
-import { Todo } from './todo';
-import { Calendar } from './calendar';
-import { CalendarService } from './calendar/calendar.service';
-import { FeedService } from './feed/feed.service';
 import { LineChartService } from './lineChart/lineChart.service';
 import { PieChartService } from './pieChart/pieChart.service';
-import { TodoService } from './todo/todo.service';
 import { TrafficChartService } from './trafficChart/trafficChart.service';
 import { UsersMapService } from './usersMap/usersMap.service';
 import { CoinTable } from './coinTable/coinTable.component';
@@ -47,18 +41,12 @@ import { XBitApiService } from '../../services/xbitapi/xbit-api.service';
     TrafficChart,
     UsersMap,
     LineChart,
-    Feed,
-    Todo,
-    Calendar,
     Dashboard,
     HashrateChart
   ],
   providers: [
-    CalendarService,
-    FeedService,
     LineChartService,
     PieChartService,
-    TodoService,
     TrafficChartService,
     UsersMapService,
     CoinTableService,
