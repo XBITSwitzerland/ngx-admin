e7389e4f1e278a863d5c84e3ac673859e8a5c956
Ofaso <kevinwuestner5@gmail.com>
2018-09-22 11:57:31 +0200

    chore(Rebase): Rebase step 3

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data/data.service';
 import { CoinMarketCapCoin } from '../../entities/coinmarketcap-coin';
-import { CoinMarketCapService } from '../../services/coinmarketcap/coinmarketcap.service'
 import { DataType } from '../../entities/enums/data-type';
 
 @Component({
@@ -12,8 +11,13 @@ import { DataType } from '../../entities/enums/data-type';
 export class Dashboard {
 
   constructor(
-    private dataService: DataService,
-    private CoinMarketCapService: CoinMarketCapService
-  ) { }
+    private dataService: DataService
+  ) {
+    dataService.coinMarketCapCoins.subscribe( res => {
+      res.forEach(element => {
+        //console.log("Coin: " + JSON.stringify(element, null, 2));
+      });
+    });
+   }
 
 }
